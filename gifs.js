6b5c70556c077a7625f06e4b99d1c7ad1a1b96fd
lowebrew <lowetide@outlook.com>
2026-09-19 14:15:22 +0000

    feat: scatter 16 retro GIFs across all pages like annoying 90's ads

---
+++ gifs.js
@@ -0,0 +1,26 @@
+(function() {
+  var gifs = [
+    'ROMANCE.gif', 'HERMAN.gif', 'ISHELLREAL.gif', 'MAN-HAPPY.gif',
+    'BECOOL.gif', 'USECOMPUTERFIGHTHORSES.gif', 'PROADAMERICAN.gif',
+    'AUTISM.gif', 'WASHHANDS.gif', 'PROTECTYOURSELF.gif',
+    'FREEONLINEBOOKS.gif', 'HAVEYOUSEENBIGBOY.gif', 'WIZARDFLUTES.gif',
+    'DANCETHEHORSESAWAY.gif', 'YOU-DESERVE-IT-ALL-BBY.gif', 'BRAINSOFT.gif'
+  ];
+
+  var container = document.createElement('div');
+  container.className = 'gif-barrage';
+
+  var path = window.location.pathname;
+  var isSubpage = path.indexOf('/reports/') !== -1 || path.indexOf('/mission/') !== -1 || path.indexOf('/contact/') !== -1 || path.indexOf('/wiki/') !== -1;
+  var prefix = isSubpage ? '../assets/' : '/assets/';
+
+  gifs.forEach(function(name) {
+    var img = document.createElement('img');
+    img.src = prefix + name;
+    img.className = 'gif-item';
+    img.alt = '';
+    container.appendChild(img);
+  });
+
+  document.body.appendChild(container);
+})();
